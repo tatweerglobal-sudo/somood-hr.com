b37fd218eaae08bbeb188466fda07eb3bded0f91
Saif Mohamed <saifmohamed1356@gmail.com>
2026-09-18 16:27:31 +0300

    Fix logo visibility across French subpages, fix root fr.html, disable wpml-cookie redirect, and add arabic slug aliases

--- wp-content/plugins/sitepress-multilingual-cms/res/js/cookies/language-cookie.js
+++ wp-content/plugins/sitepress-multilingual-cms/res/js/cookies/language-cookie.js
@@ -1,6 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-	for(var cookieName in wpml_cookies) {
-		var cookieData = wpml_cookies[cookieName];
-		document.cookie = cookieName + '=' + cookieData.value + ';expires=' + cookieData.expires + '; path=' + cookieData.path + '; SameSite=Lax';
-	}
-});
+// disabled wpml cookie to prevent overriding static language routing
